28ff59ee5e04b7fd9af07a581003a7f61df411c0
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-05-07 18:11:08 +0800

    create reusable login form component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,32 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./component/LoginForm";
+import style from "./styles.module.scss";
 
 function App() {
-
   return (
-   <h1>App</h1>
-  )
+    <div className={style.appConatiner}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <LoginForm title="Sign In" linkTitle="Sign Up" pathLocation="sign-up" />
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <LoginForm
+                title="Sign Up"
+                linkTitle="Sign In"
+                pathLocation="/"
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/component/LoginForm.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, Input } from "antd";
+import style from "./loginForm.module.scss";
+
+const LoginForm = ({ title, linkTitle, pathLocation }) => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <div className={style.loginForm}>
+        <h1>{title}</h1>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              {title}
+            </Button>
+            <Link style={{ marginLeft: "20px" }} to={pathLocation}>
+              {linkTitle}
+            </Link>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
